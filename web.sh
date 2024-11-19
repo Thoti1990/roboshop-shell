43bd688e8fcb4b99fe7808d39184e16d6d51fdf6
Thoti1990 <thotinagaraju437@gmail.com>
2024-11-19 14:44:11 +0530

    redis commited

--- web.sh
+++ web.sh
@@ -61,6 +61,6 @@ cp /home/centos/roboshop-shell/roboshop.conf /etc/nginx/default.d/roboshop.conf 
 
 VALIDATE $? "copied roboshop reverse proxy config"
 
-systemctl restart nginx  &>> $LOGFILE
+systemctl restart nginx &>> $LOGFILE
 
 VALIDATE $? "Restating nginx"
