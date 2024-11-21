65094b5eb50b16aa3e5b1dbe3dc89eafa7bdef7c
Thoti1990 <thotinagaraju437@gmail.com>
2024-11-21 14:49:48 +0530

    commited

---
+++ roboshop.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+AMI=ami-0b4f379183e5706b9
+SG_ID=sg-02307b76691a3b4be # replace with your SG ID
+INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
+
+for i in "${INSTANCES[@]}"
+do
+    echo "instance is: $i"
+    if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
+    then 
+        INSTANCE_TYPE="t3.small"
+    else
+        INSTANCE_TYPE="t2.micro"
+    fi
+   
+    aws ec2 run-instances --image-id ami-0b4f379183e5706b9 --instance-type $INSTANCE_TYPE --security-group-ids sg-02307b76691a3b4be
+
+done
+
